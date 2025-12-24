3e318f287ef9c75f74dd6c69b70d02f3e0d825c3
Johhannas Reyn <johhannasreyn@gmail.com>
2025-12-24 14:12:11 -0800

    Update Markdown2Dir (processors, settings, examples)

---
+++ tests/project/sub_dir/onfence_file.hpp
@@ -0,0 +1,5 @@
+// This is a test C++ header using on_fence convention
+class TestClass {
+public:
+    TestClass() {}
+};
---
+++ tests/project/sub_dir/postfence_file.cpp
@@ -0,0 +1,5 @@
+// project/sub_dir/postfence_file.cpp
+// This is a test C++ file using after_fence convention
+void test_function() {
+    // Implementation
+}
